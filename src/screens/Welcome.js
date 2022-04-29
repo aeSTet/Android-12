9d6055121366c30fc3c6a7ca3348073a5dcd9338
aeSTet <76133815+aeSTet@users.noreply.github.com>
2022-04-29 04:57:36 +0300

    Add files via upload

---
+++ src/screens/Welcome.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+export default class Welcome extends React.Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    height: '100%',
+                    padding: 40,
+                    paddingTop: 50,
+                    backgroundColor: 'white'
+                }}>
+                <View
+                    style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginBottom: 30,
+                    }}>
+                    <Image
+                        source={require('../images/image.jpg')}
+                        style={{
+                            width: 150,
+                            height: 150,
+                        }}
+                    />
+                </View>
+                <Text
+                    style={{
+                        fontSize: 30,
+                        fontWeight: 'bold',
+                        alignSelf: 'center',
+                    }}>
+                    Добро пожаловать!
+                </Text>
+                <Text
+                    style={{
+                        textAlign: 'center',
+                        color: 'grey',
+                        marginBottom: 50,
+                    }}>
+                    Вы вошли в систему как {'\n' + this.props.route.params.email}
+                </Text>
+            </View>
+        );
+    }
+}
